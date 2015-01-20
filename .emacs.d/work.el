67ee5f593e9a5fb0ed576bc090004f8628bb501e
James McCoy <vega.james@gmail.com>
2015-01-20 10:23:24 -0500

    Fix set up of work-specific indentation settings

--- .emacs.d/work.el
+++ .emacs.d/work.el
@@ -1,9 +1,9 @@
 ; Settings specific for work projects
-(add-hook 'dtrt-indent-mode-hook
-          (lambda ()
-            (when (and (buffer-file-name)
-                       (string-match "safe/catmerge" (buffer-file-name)))
-              (c-set-style "stroustrup")
-              (setq indent-tabs-mode t)
-              (setq tab-width 4)
-              (setq c-basic-offset 4))))
+(defun daq-indent-settings ()
+  (when (and (buffer-file-name)
+             (string-match "safe/catmerge" (buffer-file-name)))
+    (c-set-style "stroustrup")
+    (setq indent-tabs-mode t)
+    (setq tab-width 4)
+    (setq c-basic-offset 4)))
+(add-hook 'c-mode-common-hook 'daq-indent-settings)
